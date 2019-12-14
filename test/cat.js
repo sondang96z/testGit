345e4c72bd2cfb46950138faa4b2a9b4e699aa1b
sondang96z <sondang96z@gmail.com>
2019-12-14 23:51:28 +0700

    update Cat sleep

--- test/cat.js
+++ test/cat.js
@@ -2,4 +2,8 @@ class Cat {}
 Cat.prototype.run = function(params) {
 	console.log(running);
 };
+<<<<<<< HEAD
 Cat.prototype.eat = function(param) {};
+=======
+Cat.prototype.sleep = function(param) {};
+>>>>>>> update Cat sleep

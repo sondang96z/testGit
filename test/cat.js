6507922ac39353943e63906d30c5c8e32595717b
sondang96z <sondang96z@gmail.com>
2019-12-15 00:21:30 +0700

    add eat and sleep info

--- test/cat.js
+++ test/cat.js
@@ -1,8 +1,12 @@
 class Cat {}
 Cat.prototype.run = function(params) {
-	console.log(running);
+	console.log('running');
 };
 
-Cat.prototype.eat = function(param) {};
+Cat.prototype.eat = function(param) {
+	console.log('Eating');
+};
 
-Cat.prototype.sleep = function(param) {};
+Cat.prototype.sleep = function(param) {
+	console.log('Sleep');
+};

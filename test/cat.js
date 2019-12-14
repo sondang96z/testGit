924542e56a6beddcda10d3b4332612065753b8f3
sondang96z <sondang96z@gmail.com>
2019-12-15 00:23:56 +0700

    master update eat and sleep function

--- test/cat.js
+++ test/cat.js
@@ -3,6 +3,10 @@ Cat.prototype.run = function(params) {
 	console.log(running);
 };
 
-Cat.prototype.eat = function(param) {};
+Cat.prototype.eat = function(param) {
+	console.log('eat');
+};
 
-Cat.prototype.sleep = function(param) {};
+Cat.prototype.sleep = function(param) {
+	console.log('sleepp');
+};

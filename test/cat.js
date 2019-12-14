11b351bf06355c357e47c1cd7142f4eb380e545e
sondang96z <sondang96z@gmail.com>
2019-12-15 00:37:11 +0700

    cat update

--- test/cat.js
+++ test/cat.js
@@ -5,8 +5,10 @@ Cat.prototype.run = function(params) {
 
 Cat.prototype.eat = function(param) {
 	console.log('Eating');
+	console.log('123');
 };
 
 Cat.prototype.sleep = function(param) {
 	console.log('Sleep');
+	console.log('321');
 };

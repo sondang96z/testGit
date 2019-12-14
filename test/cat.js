db45fb6b540c357c1a51f4b56f737fecf9e7ea97
sondang96z <sondang96z@gmail.com>
2019-12-15 01:05:39 +0700

    123

--- test/cat.js
+++ test/cat.js
@@ -7,10 +7,12 @@ Cat.prototype.eat = function(param) {
 	console.log('Eating');
 	console.log('123');
 	console.log('test1');
+	console.log('abc');
 };
 
 Cat.prototype.sleep = function(param) {
 	console.log('Sleep');
 	console.log('321');
 	console.log('test2');
+	console.log('def');
 };
